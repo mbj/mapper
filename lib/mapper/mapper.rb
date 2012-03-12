ee3d5ad077f9052cdef7a5ea82d8cbca5632f026
Markus Schirp <mbj@seonic.net>
2012-03-12 15:33:19 +0100

    Use correct NotImplementedError constant

--- lib/mapper/mapper.rb
+++ lib/mapper/mapper.rb
@@ -1,11 +1,11 @@
 module Mapper
   class Mapper
     def dump(object)
-      raise NotImplemented,'must be implemented in subclasses'
+      raise NotImplementedError,'must be implemented in subclasses'
     end
    
     def load(dump)
-      raise NotImplemented,'must be implemented in subclasses'
+      raise NotImplementedError,'must be implemented in subclasses'
     end
   end
 end
