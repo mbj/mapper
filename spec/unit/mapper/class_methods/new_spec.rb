66c400482ec46014a142f8ad6bfab8f0ed44d4e0
Markus Schirp <mbj@seonic.net>
2012-07-05 00:26:04 +0200

    Test for objects beeing frozen

---
+++ spec/unit/mapper/class_methods/new_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Mapper,'.new' do
+  subject { object.new(model,attributes) }
+
+  let(:object)     { described_class }
+
+  let(:model)      { mock('Model') }
+  let(:attributes) { mock('Attributes') }
+
+  it { should be_frozen }
+end
